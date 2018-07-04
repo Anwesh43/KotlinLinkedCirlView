05a5abbcec11f91ec5cbb1fcfb7d5fd6247c2f92
Anwesh43 <anweshthecool0@gmail.com>
2018-07-05 01:24:27 +0530

    adding LinkedCirlView class

--- linkedcirlview/src/main/java/com/anwesh/uiprojects/linkedcirlview/LinkedCirlView.kt
+++ linkedcirlview/src/main/java/com/anwesh/uiprojects/linkedcirlview/LinkedCirlView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.linkedcirlview
 /**
  * Created by anweshmishra on 05/07/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+class LinkedCirlView (ctx : Context) : View (ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

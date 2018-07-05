c1a89c16afb2ead8580d793e3c3d29808235292b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-05 10:09:12 +0530

    adding listener in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirlview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirlview/MainActivity.kt
@@ -2,12 +2,18 @@ package com.anwesh.uiprojects.kotlinlinkedcirlview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedcirlview.LinkedCirlView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedCirlView.create(this)
+        val view  : LinkedCirlView = LinkedCirlView.create(this)
+        view.addListener({showShortToast("animation ${it} is completed")}, {showShortToast("animation ${it} is reset")})
     }
+}
+
+fun MainActivity.showShortToast(text : String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }

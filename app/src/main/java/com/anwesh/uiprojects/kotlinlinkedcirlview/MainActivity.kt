e1a39e3a6dc09fd7983a4c263c6a8b26997b6338
Anwesh43 <anweshthecool0@gmail.com>
2018-07-05 10:10:41 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirlview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirlview/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.anwesh.uiprojects.kotlinlinkedcirlview
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import com.anwesh.uiprojects.linkedcirlview.LinkedCirlView
 
@@ -11,9 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view  : LinkedCirlView = LinkedCirlView.create(this)
         view.addListener({showShortToast("animation ${it} is completed")}, {showShortToast("animation ${it} is reset")})
+        fullScreen()
     }
 }
 
 fun MainActivity.showShortToast(text : String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+fun MainActivity.fullScreen() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
